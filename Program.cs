b6a361a2d900ccc4a74cc86f945f9b8e01b7db12
onteriowright <onteriowright@yahoo.com>
2020-02-28 10:36:22 -0600

    added loops and objects

--- Program.cs
+++ Program.cs
@@ -7,22 +7,20 @@ namespace ListIntro
   {
     static void Main(string[] args)
     {
-      List<string> nssCohorts = new List<string>
-            {
-                "Cohort 36",
-                "Cohort 37",
-                "Cohort 38"
-            };
+      List<string> nssCohorts = new List<string> {
+        "Cohort 36",
+        "Cohort 37",
+        "Cohort 38"
+      };
 
       nssCohorts.Add("Cohort 39");
 
-      List<int> birthYears = new List<int>
-            {
-                1996,
-                1992,
-                1997,
-                2001
-            };
+      List<int> birthYears = new List<int> {
+        1996,
+        1992,
+        1997,
+        2001
+      };
 
       birthYears.Add(1999);
       birthYears.Add(2005);
@@ -44,8 +42,22 @@ namespace ListIntro
           Console.WriteLine("Bulls won the Championship🏀");
         }
       }
-      {
 
+      // Dictionaries
+      Dictionary<string, string> student = new Dictionary<string, string>();
+
+      student.Add("Name", "Adam");
+      student.Add("Cohort", "Day 37");
+
+      Dictionary<string, string> anotherStudent = new Dictionary<string, string>()
+      {
+        { "Name", "Brenda" },
+        { "Cohort", "Day 37" }
+      };
+
+      foreach (KeyValuePair<string, string> pair in student)
+      {
+        Console.WriteLine($"The student's {pair.Key} is {pair.Value}");
       }
 
     }
